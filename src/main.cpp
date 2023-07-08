315ad5de79ff2c1af6549ebe8d5517296be2cc45
Swaraj Ketan Santra <swarajksantra@gmail.com>
2023-07-08 21:30:35 +0530

    initial

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "product.h"
+#include "order.h"
+#include "invoice.h"
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Welcome to IMS" << std::endl;
+    std::cin.get();
+
+    return 0;
+}
